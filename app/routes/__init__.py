3e856cb29f39abc83159fc0d59a14a50db15a306
N1SL <nils.herterich@gmail.com>
2022-03-26 10:41:29 +0100

    create handler for picking an image and set parameters for color jitter transformation #3

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,5 @@
 from .classifications import *
 from .classifications_id import *
+from .transformations import *
 from .info import *
 from .home import *
---
+++ app/routes/transformations.py
@@ -0,0 +1,44 @@
+from flask import render_template
+
+from app import app
+from app.forms.transformation_form import TransformationForm
+from ml.transformation_utils import transform_image, save_image
+from config import Configuration
+
+config = Configuration()
+
+@app.route('/transformations', methods=['GET', 'POST'])
+def transformations():
+    """API for selecting a an image and running a
+    transformation job. Returns the output image from the
+    transformation."""
+    form = TransformationForm()
+    if form.validate_on_submit():  # POST
+
+        brightness = form.brightness.data
+        contrast = form.contrast.data
+        saturation = form.saturation.data
+        hue = form.hue.data
+
+        image_id = form.image.data
+
+        img, edit_image_id = transform_image(brightness, contrast, saturation, hue, image_id)
+
+        save_image(edit_image_id, img)
+
+        print("SUBMIT")
+
+        # returns the image classification output from the specified model
+        # return render_template('classification_output.html', image_id=image_id, results=result_dict)
+        return render_template("transformation_output.html", image_id=image_id, edit_image_id=edit_image_id)
+
+    # otherwise, it is a get request and should return the
+    # image and model selector
+    return render_template('transformation_select.html', form=form)
+
+""""
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+"""
